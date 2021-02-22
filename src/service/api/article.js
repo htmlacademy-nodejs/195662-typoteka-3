8ca54871a11c2601c0a1a11da689f1f60e3ba6bc
against-v <against.vegetables@gmail.com>
2021-02-23 00:27:43 +0300

    api: articles

---
+++ src/service/api/article.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const {Router} = require(`express`);
+const {HttpCode} = require(`../../constants`);
+
+const route = new Router();
+
+module.exports = (app, articleService) => {
+  app.use(`/articles`, route);
+
+  route.get(`/`, (req, res) => {
+    const categories = articleService.findAll();
+    res.status(HttpCode.OK).json(categories);
+  });
+};
