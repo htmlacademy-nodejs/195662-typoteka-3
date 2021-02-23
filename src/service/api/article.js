e14feb02b076a050fd8a8cd2c1a93d278a501880
against-v <against.vegetables@gmail.com>
2021-02-23 15:24:09 +0300

    api: get comments request

--- src/service/api/article.js
+++ src/service/api/article.js
@@ -3,15 +3,16 @@
 const {Router} = require(`express`);
 const {HttpCode} = require(`../../constants`);
 const articleValidator = require(`../middlewares/article-validator`);
+const articleExists = require(`../middlewares/article-exists`);
 
 const route = new Router();
 
-module.exports = (app, articleService) => {
+module.exports = (app, articleService, commentService) => {
   app.use(`/articles`, route);
 
   route.get(`/`, (req, res) => {
-    const categories = articleService.findAll();
-    res.status(HttpCode.OK).json(categories);
+    const articles = articleService.findAll();
+    res.status(HttpCode.OK).json(articles);
   });
 
   route.get(`/:articleId`, (req, res) => {
@@ -22,12 +23,12 @@ module.exports = (app, articleService) => {
       return res.status(HttpCode.NOT_FOUND).send(`Not found with ${articleId}`);
     }
 
-    return res.status(HttpCode.OK).send(article);
+    return res.status(HttpCode.OK).json(article);
   });
 
   route.post(`/`, articleValidator, (req, res) => {
     const article = articleService.create(req.body);
-    return res.status(HttpCode.CREATED).send(article);
+    return res.status(HttpCode.CREATED).json(article);
   });
 
   route.put(`/:articleId`, articleValidator, (req, res) => {
@@ -51,6 +52,14 @@ module.exports = (app, articleService) => {
       return res.status(HttpCode.NOT_FOUND).send(`Not found with ${articleId}`);
     }
 
-    return res.status(HttpCode.OK).send(article);
+    return res.status(HttpCode.OK).json(article);
+  });
+
+  route.get(`/:articleId/comments`, articleExists(articleService), (req, res) => {
+    const {article} = res.locals;
+
+    const comments = commentService.findAll(article);
+
+    res.status(HttpCode.OK).json(comments);
   });
 };
