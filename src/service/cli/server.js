e608973780cad1ab007ac3424371775696566335
against-v <against.vegetables@gmail.com>
2021-03-01 00:05:53 +0300

    add logger

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -11,11 +11,26 @@ const app = express();
 const logger = getLogger({name: `api`});
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  logger.debug(`Request on route ${req.url}`);
+  res.on(`finish`, () => {
+    logger.info(`Response status code ${res.statusCode}`);
+  });
+  next();
+});
+
 app.use(API_PREFIX, routes);
 
-app.use((req, res) => res
-  .status(HttpCode.NOT_FOUND)
-  .send(`Not found`));
+app.use((req, res) => {
+  res.status(HttpCode.NOT_FOUND).send(`Not found`);
+  logger.error(`Route not found: ${req.url}`);
+});
+
+app.use((err, _req, _res, _next) => {
+  logger.error(`An error occured on processing request: ${err.message}`);
+});
+
 
 module.exports = {
   name: `--server`,
