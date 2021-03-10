ca81098d84c304521a0239591f422e9f731d3683
against-v <against.vegetables@gmail.com>
2021-03-10 22:28:46 +0300

    4.3: throw articles in admin/publications, update admin/publications template

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -2,8 +2,12 @@
 
 const {Router} = require(`express`);
 const myRouter = new Router();
+const api = require(`../api`).getAPI();
 
-myRouter.get(`/`, (req, res) => res.render(`admin/publications`));
+myRouter.get(`/`, async (req, res) => {
+  const articles = await api.getArticles();
+  return res.render(`admin/publications`, {articles});
+});
 myRouter.get(`/comments`, (req, res) => res.render(`admin/comments`));
 
 module.exports = myRouter;
