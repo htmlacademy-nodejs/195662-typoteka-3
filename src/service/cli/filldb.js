99c98561076be864cc7cfb03df077e3ccdf709bb
against-v <against.vegetables@gmail.com>
2021-05-17 23:14:53 +0300

    init-db

--- src/service/cli/filldb.js
+++ src/service/cli/filldb.js
@@ -2,6 +2,7 @@
 
 const sequelize = require(`../lib/sequelize`);
 const defineModels = require(`../models`);
+const initDatabase = require(`../lib/init-db`);
 const Aliase = require(`../models/aliase`);
 const {getLogger} = require(`../lib/logger`);
 const {readContent, getRandomInt, shuffle} = require(`../../utils`);
@@ -84,36 +85,6 @@ module.exports = {
     ];
     const articles = generateArticles(...generateArticlesParams);
 
-    const {User, Article, Category} = defineModels(sequelize);
-    await sequelize.sync({force: true});
-
-    /* Таблица users */
-    await User.bulkCreate(
-        users.map((item) => {
-          return {
-            email: item.email,
-            firstname: item.firstname,
-            lastname: item.lastname,
-            password: item.password,
-            avatar: item.avatar,
-          };
-        })
-    );
-    /* Таблица categories */
-    await Category.bulkCreate(
-        categories.map((item) => {
-          return {
-            title: item
-          };
-        })
-    );
-    /* Таблица articles, comments и articles_categories*/
-    const articlePromises = articles.map(async (article) => {
-      const articleModel = await Article.create(article, {
-        include: [Aliase.COMMENTS]
-      });
-      articleModel.addCategories(article.categories);
-    });
-    await Promise.all(articlePromises);
+    return initDatabase(sequelize, {users, categories, articles});
   }
 };
